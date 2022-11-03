b6ab49d0c9b664f85e7761f30b6cd7509183efa3
dericklopis <stanforever08@gmail.com>
2022-11-03 20:41:14 +0530

    Adding ineheritance

---
+++ OOPSdEMO/Inheritance/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSdEMO.Inheritance
+{
+    class Animal
+    {
+        public string name;
+        public void Sleep()
+        {
+            Console.WriteLine("Sleeping");
+        }
+        public void Run()
+        {
+            Console.WriteLine("Depends on child");
+        }
+    }
+}
---
+++ OOPSdEMO/Program.cs
@@ -0,0 +1,34 @@
+﻿using OOPSdEMO.Inheritance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSdEMO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose one program from below options");
+            Console.WriteLine("1:Inheritance\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Animal animal = new Animal();
+                    animal.Sleep();
+                    lion lion = new lion();
+                    lion.name ="lion";
+                    Console.WriteLine("Name: "+ lion.name);
+                    break;
+                default:
+                    Console.WriteLine("Please choose numbers within range");
+                    break;
+            }
+        }
+    }
+}
